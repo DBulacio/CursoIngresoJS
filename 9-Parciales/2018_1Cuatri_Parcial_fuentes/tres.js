4a0573272e5e65d3d33e36b028f650e7af41d1d8
DBulacio <44949670+DBulacio@users.noreply.github.com>
2019-07-11 11:24:53 -0300

    Ejercicios de parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+  var precio, porcentaje;
 
+  precio     = prompt("Ingrese el precio.")
+  porcentaje = prompt("Ingrese el porcentaje.")
+
+  precio     = parseInt(precio);
+  porcentaje = parseInt(porcentaje);
+
+  precio -= precio * porcentaje;
+
+  document.getElementById("elPrecioFinal").value = precio;
+
+  //Esta mal, rehacer
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
+  var largo, ancho, p;
 
+  largo = prompt("Ingrese el largo del rectangulo");
+  ancho = prompt("Ingrese el ancho del rectangulo");
+
+  p = (largo + ancho) * 2;
+  alert("El perimetro es " + p);
 }
