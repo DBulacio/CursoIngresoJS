9ac187d9badfc96c8d303156f0673e1651bef6ed
DBulacio <44949670+DBulacio@users.noreply.github.com>
2019-07-11 10:15:18 -0300

    Todo Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,43 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+  var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+  switch(mesDelAño){
+    case "Enero":
+      alert("tiene 31 días");
+      break;
+    case "Febrero":
+      alert("Este mes no tiene más de 29 días.");
+      break;
+    case "Marzo":
+      alert("tiene 31 días");
+      break;
+    case "Abril":
+      alert("tiene 30 días");
+      break;
+    case "Mayo":
+      alert("tiene 31 días");
+      break;
+    case "Junio":
+      alert("tiene 30 días");
+      break;
+    case "Julio":
+      alert("tiene 31 días");
+      break;
+    case "Agosto":
+      alert("tiene 31 días");
+      break;
+    case "Septiembre":
+      alert("tiene 30 días");
+      break;
+    case "Octubre":
+      alert("tiene 31 días");
+      break;
+    case "Noviembre":
+      alert("tiene 30 días");
+      break;
+    case "Diciembre":
+      alert("tiene 31 días");
+      break;
+  }
+}
