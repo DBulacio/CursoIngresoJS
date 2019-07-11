48529be71a3548139437514711d5d61731e9cc21
DBulacio <44949670+DBulacio@users.noreply.github.com>
2019-07-11 09:19:37 -0300

    E/S

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -1,8 +1,15 @@
 /*Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
+transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var num1, num2, resultado;
+
+  num1 = parseInt(document.getElementById("numeroDividendo").value);
+  num2 = parseInt(document.getElementById("numeroDivisor").value);
+
+  resultado = num1 % num2;
+
+  alert("El resto es " + resultado);
 }
